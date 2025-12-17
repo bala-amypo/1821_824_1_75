de9b65dac91b4253c661f3a6df1e041c75a2c0c3
bala-amypo <bala@amypo.in>
2025-12-17 08:20:18 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,7 +1,7 @@
 package com.example.demo.model;
 
-import.jakarta.persistence.Entity;
-import.jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import.jakarta.persistence.PrePersist;
 
 @Entity
