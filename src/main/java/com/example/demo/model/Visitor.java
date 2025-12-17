a93a17c5f1591dbafc815f79ae36cc97d2b56fee
bala-amypo <bala@amypo.in>
2025-12-17 05:17:50 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,4 +1,15 @@
-import.jakarta
+import.jakarta.persistence.Entity;
+import.jakarta.persistence.Id;
+
+@Entity
 public class Visitor{
+    @Id
+    private Long id;
+    private String fulllName;
+    private String email;
+    private String phone;
+    private String idProof;
+    private LocalDateTime createdAt;
 
 }
+public
