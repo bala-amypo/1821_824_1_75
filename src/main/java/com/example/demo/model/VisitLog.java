8f98e547decbd8214343987ec0a1494d36f7295e
bala-amypo <bala@amypo.in>
2025-12-17 05:42:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/VisitLog.java
@@ -0,0 +1,61 @@
+import.jakarta.persistence.Entity;
+import.jakarta.persistence.Id;
+
+@Entity
+public class Visitor{
+    @Id
+    private Long id;
+    private String fulllName;
+    private String email;
+    private String phone;
+    private String idProof;
+    private LocalDateTime createdAt;
+
+}
+public getid()
+{
+    return id;
+}
+public setid(Long id)
+{
+    this.id=id;
+}
+public getFname()
+{
+    return Fname;
+}
+public setFname(String Fname)
+{
+    this.Fname=Fname;
+}
+public getemail(){
+    return email;
+}
+public setemail(String email)
+{
+    this.email=email;
+}
+public getphone()
+{
+    return phone;
+}
+public setphone(String phone)
+{
+    this.phone=phone;
+}
+public getidp()
+{
+    return idp;
+}
+public setidp(String idp)
+{
+    this.idp=idp;
+}
+public getcat()
+{
+    return cat;
+}
+public setcat(LocalDateTime)
+{
+    this.cat=cat;
+}
