24c10b59821b90aa21c68bd0ee823ef9ee7eb19c
bala-amypo <bala@amypo.in>
2025-12-18 10:25:05 +0000

    User commit

--- src/main/java/com/example/demo/Service/ScoreAuditLog.java
+++ src/main/java/com/example/demo/Service/ScoreAuditLog.java
@@ -1,8 +1,6 @@
 package com.example.demo.service;
-import java.util.List;
 import com.example.demo.entity.ScoreAuditLog;
 public interface ScoreAuditLog{
     ScoreAuditLog logScoreChange(Long visitorId,Long ruleId,ScoreAuditLog log);
-    List<ScoreAuditLog>getLogByVisitor(Long visitorId);
-    ScoreAuditLog getLog(Long id);
+    
 }
--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -1,10 +1,9 @@
 package com.example.demo.service;
-import com.example.demo.AuthRequest;
 
 import com.example.demo.RegisterRequest;
 import com.example.demo.entity.User;
 public interface UserService{
     User register(RegisterRequest request);
-    AuthResponse login(AuthRequest request);
+    User login(AuthRequest request);
     User getByEmail(String email);
 }
