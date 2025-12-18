a122b49584422070f81d49ae043bb3bcb3044cae
bala-amypo <bala@amypo.in>
2025-12-18 09:20:05 +0000

    User commit

--- src/main/java/com/example/demo/Service/RiskRuleService.java
+++ src/main/java/com/example/demo/Service/RiskRuleService.java
@@ -1,8 +1,8 @@
 package com.example.demo.service;
 import java.utio.List;
-import com.example.demo.entity.Visitor;
-public interface VisirService{
-    Visitor createVisitor(Visitor visitor);
-    Visitor getVisitor(Long id);
-    List<Visitor>getAllVisitors();
+import com.example.demo.entity.RiskRule;
+public interface RiskRuleService{
+    RiskRule createRule(RiskRule Rule);
+    List<RiskRule>getAllRules();
+    RiskRule getRule(Long id);
 }
---
+++ src/main/java/com/example/demo/Service/RiskScoreService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+import java.utio.List;
+import com.example.demo.entity.RiskScore;
+public interface RiskScoreService{
+    RiskScore evaluatedVisitor(Long visitorId);
+    RiskScore getScoreForVisitor(Long visitorId);
+    List<RiskScore>getAllScores();
+}
