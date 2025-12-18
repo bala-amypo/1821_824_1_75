27efdb8b8767a359706919844b0d84cb11969298
bala-amypo <bala@amypo.in>
2025-12-18 05:31:05 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskScore.java
+++ src/main/java/com/example/demo/model/RiskScore.java
@@ -3,7 +3,8 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakrata.persistence.GenerationType;
+import java.time.LocalDateTime;
 
 @Entity
 public class RiskScore{
