14d323dca8d0019ba150104ec4b15d5967c03b1f
bala-amypo <bala@amypo.in>
2025-12-18 05:01:05 +0000

    User commit

--- src/main/java/com/example/demo/model/VisitLog.java
+++ src/main/java/com/example/demo/model/VisitLog.java
@@ -1,61 +1,59 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.PrePersist;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import java.time.LocalDateTime;
-
-
-// @Entity
-// public class Visitor{
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     private LocalDateTime entryTime;
-//     private LocalDateTime exitTime;
-//     private String purpose;
-//     private String location;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
 
 
-// public Long getid(){
-//     return id;
-// }
-// public void setid(Long id){
-//     this.id=id;
-// }
-// public  LocalDateTime getentryTime(){
-//     return entryTime;
-// }
-// public void setemail(LocalDateTime entryTime){
-//     this.entryTime=entryTime;
-// }
-// public LocalDateTime getexitTime(){
-//     return exitTime;
-// }
-// public void setphone(LocalDateTime exitTime){
-//     this.exitTime=exitTime;
-// }
-// public String getpurpose(){
-//     return purpose;
-// }
-// public void setpurpose(String purpose){
-//     this.purpose=purpose;
-// }
-// public String getlocation(){
-//     return location;
-// }
-// public void setlocation(String location){
-//     this.location=location;
-// }
-//  VisitLog()
-// {}
-//  VisitLog(Long id,LocalDateTime entryTime,LocalDateTime exitTime,String purpose,String location){
-//     this.id=id;
-//     this.entryTime=entryTime;
-//     this.exitTime=exitTime;
-//     this.purpose=purpose;
-//     this.location=location;
-// }
-// }
+@Entity
+public class Visitor{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime entryTime;
+    private LocalDateTime exitTime;
+    private String purpose;
+    private String location;
+
+
+public Long getId(){
+    return id;
+}
+public void setId(Long id){
+    this.id=id;
+}
+public  LocalDateTime getEntryTime(){
+    return entryTime;
+}
+public void setEntryTime(LocalDateTime entryTime){
+    this.entryTime=entryTime;
+}
+public LocalDateTime getExitTime(){
+    return ExitTime;
+}
+public void setExitTime(LocalDateTime exitTime){
+    this.exitTime=exitTime;
+}
+public String getPurpose(){
+    return purpose;
+}
+public void setPurpose(String purpose){
+    this.purpose=purpose;
+}
+public String getLocation(){
+    return location;
+}
+public void setLocation(String location){
+    this.location=location;
+}
+public VisitLog(){}
+public VisitLog(Long id,LocalDateTime entryTime,LocalDateTime exitTime,String purpose,String location){
+    this.id=id;
+    this.entryTime=entryTime;
+    this.exitTime=exitTime;
+    this.purpose=purpose;
+    this.location=location;
+}
+}
