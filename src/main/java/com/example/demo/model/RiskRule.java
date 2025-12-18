c48c0e6fa152b94182f864896215462594d741d0
bala-amypo <bala@amypo.in>
2025-12-18 05:46:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/RiskRule.java
@@ -0,0 +1,10 @@
+package com.example.demao.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
+
+@Entity
+public cls
