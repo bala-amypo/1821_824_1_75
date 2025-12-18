8bb7ec64127b4e3dc71d44d7905a8ba7dbc5be03
bala-amypo <bala@amypo.in>
2025-12-18 07:55:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/VisitorLogRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.VisitorLog;
+public interface VisitorLogRepository extends JpaRepository<Visitor,Long>{
+    
+}
