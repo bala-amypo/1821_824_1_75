a494fad08be67537c7a74e504eb3de356fc13470
bala-amypo <bala@amypo.in>
2025-12-19 04:27:19 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskScoreRepository.java
+++ src/main/java/com/example/demo/repository/RiskScoreRepository.java
@@ -1,7 +1,7 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.example.demo.entity.RiskScore;
-// public interface RiskScoreRepository extends JpaRepository<RiskScore,Long>{
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.RiskScore;
+public interface RiskScoreRepository extends JpaRepository<RiskScore,Long>{
     
-// }
+}
