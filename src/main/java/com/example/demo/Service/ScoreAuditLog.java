753941c4de06248a8145e25c9a433e6c2ac389e2
bala-amypo <bala@amypo.in>
2025-12-19 04:18:03 +0000

    User commit

--- src/main/java/com/example/demo/Service/ScoreAuditLog.java
+++ src/main/java/com/example/demo/Service/ScoreAuditLog.java
@@ -1,6 +1,6 @@
-// package com.example.demo.service;
-// import com.example.demo.entity.ScoreAuditLog;
-// public interface ScoreAuditLog{
-//     ScoreAuditLog logScoreChange(Long visitorId,Long ruleId,ScoreAuditLog log);
+package com.example.demo.service;
+import com.example.demo.entity.ScoreAuditLog;
+public interface ScoreAuditLog{
+    ScoreAuditLog logScoreChange(Long visitorId,Long ruleId,ScoreAuditLog log);
     
-// }
+}
--- src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
+++ src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
@@ -1,7 +1,7 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.example.demo.entity.ScoreAuditLog;
-// public interface ScoreAuditLogRepository extends JpaRepository<ScoreAuditLog,Long>{
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.ScoreAuditLog;
+public interface ScoreAuditLogRepository extends JpaRepository<ScoreAuditLog,Long>{
     
-// }
+}
