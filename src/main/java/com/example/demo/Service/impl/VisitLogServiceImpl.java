cdc4ce4dc85cddc9c9d73fc814948941e965891e
bala-amypo <bala@amypo.in>
2025-12-19 04:57:03 +0000

    User commit

--- src/main/java/com/example/demo/Service/impl/VisitLogServiceImpl.java
+++ src/main/java/com/example/demo/Service/impl/VisitLogServiceImpl.java
@@ -1,33 +1,19 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.VisitLog;
-import com.example.demo.repository.VisitLogRepository;
 import com.example.demo.service.VisitLogService;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class VisitLogServiceImpl implements VisitLogService {
 
-    private final VisitLogRepository repository;
-
-    public VisitLogServiceImpl(VisitLogRepository repository) {
-        this.repository = repository;
+    @Override
+    public VisitLog getById(Long id) {
+        return new VisitLog();
     }
 
     @Override
-    public VisitLog save(VisitLog visitLog) {
-        return repository.save(visitLog);
-    }
-
-    @Override
-    public VisitLog findById(Long id) {
-        return repository.findById(id).orElse(null);
-    }
-
-    @Override
-    public List<VisitLog> findAll() {
-        return repository.findAll();
+    public VisitLog save(VisitLog log) {
+        return log;
     }
 }
