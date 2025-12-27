9bd9aecc1cec04f4c7e936b0e28384c2807659a2
bala-amypo <bala@amypo.in>
2025-12-27 05:20:32 +0000

    User commit

--- src/main/java/com/example/demo/Controller/AuthController.java
+++ src/main/java/com/example/demo/Controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    @Autowired
+   
 
     private final UserService userService;
 
